fa4d97b6fc4d9f779cd1e89df4461c38bcc24d35
said-alisic <saidalisic@gmail.com>
2021-02-21 21:42:40 +0100

    working on BookingModel.js

--- backend/models/Booking/BookingModel.js
+++ backend/models/Booking/BookingModel.js
@@ -1,4 +1,20 @@
 import mongoose from 'mongoose';
 
 
+export const BookingSchema = new mongoose.Schema(
+    { 
+        student_id: {
+            type: Number,
+            required: true,
+        },
+        class_id: {
+            type: Number,
+            required: true,
+        },
+        status: {
+            type: String,
+            required: true,
+        },
 
+
+});
