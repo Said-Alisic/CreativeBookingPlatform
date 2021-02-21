151fa29ed1cbd3e2f9e172fb65b122add0d0cbf7
said-alisic <saidalisic@gmail.com>
2021-02-21 22:23:26 +0100

    Finished ClassModel.js

--- backend/models/Class/ClassModel.js
+++ backend/models/Class/ClassModel.js
@@ -1,4 +1,26 @@
+import { createHook } from 'async_hooks';
+import { createCipher } from 'crypto';
 import mongoose from 'mongoose';
+import { TeacherSchema } from '../Teacher/TeacherModel';
 
 
-
+export const ClassSchema = new mongoose.Schema({
+    teacher_id: {
+        type: String,
+        ref: 'Teacher',
+        required: true,
+    },
+    class_name: {
+        type: String,
+        required: true,
+    },
+    date: {
+        type: Date,
+        required: true,
+    },
+    max_students: {
+        type: String,
+        required: true,
+    },
+    
+})
