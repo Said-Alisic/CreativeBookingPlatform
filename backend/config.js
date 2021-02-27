2bf94a0005de816c283bbdcf48d4565a81b84e99
said-alisic <saidalisic@gmail.com>
2021-02-27 12:54:01 +0100

    separated sequalize dbs configuration

--- backend/config.js
+++ backend/config.js
@@ -1,33 +1,9 @@
-import mysql from 'mysql2';
-import { Sequelize } from 'sequelize';
-
-
 const env = process.env;
-
-const db_host = 'localhost'
-const db = 'CreativeBookingDB'
-const db_user = 'testuser'
-const db_pass = 'testpass'
-const db_dialect = 'mysql'
-
-// Sequelize database connection
-const sequelize = new Sequelize(db, db_user, db_pass, {
-  host: db_host,
-  dialect: db_dialect
-});
-
-sequelize.authenticate( (err) => {
-  if (err) {
-    console.log(`Could not connect to database: ${db}`)
-    throw err
-  }
-  console.log(`Successfully connected to database: ${db}`)
-});
 
 // Exporting configurations for server
 export default {
     PORT: env.PORT || 4000,
-    db_connection: sequelize,
+
 };
 
 
--- backend/models/StudentModel.js
+++ backend/models/StudentModel.js
@@ -1,4 +1,16 @@
 // Holds information about students
+import sequelize from '../db_config.js'
+import { DataTypes } from 'sequelize';
 
 
+export const StudentModel = sequelize.define('Student', {
+    firstName: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    lastName: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+});
 
