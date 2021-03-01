1e07c8b753012bed41a10ca50a8c50624b6053bb
said-alisic <saidalisic@gmail.com>
2021-03-01 01:52:56 +0100

    Struggling with TypeScript interfaces :D

---
+++ frontend/src/Components/Teacher/teacherList.tsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import { getTeachers } from '../../Api/teacherApi';
+
+/** 
+interface Teacher {
+    id: number,
+    display_name: string,
+    first_name: string,
+    last_name: string,
+    email: string,
+    password: string,
+    address: string,
+    phone_number: string,
+    teacher_inf: string,
+    date_joined: string,
+    last_active: string,
+    access_level: string
+}
+**/
+ 
+
+export const TeacherList = () => {
+
+
+    return (
+        <div>
+        
+        </div>
+    )
+    
+}
+
--- frontend/src/api/teacherApi.tsx
+++ frontend/src/api/teacherApi.tsx
@@ -1,17 +1,15 @@
-import React, { useState, useEffect } from 'react'
+import React, { useEffect } from 'react'
 import axios from 'axios';
 
 const URL = `http://localhost:4000/api`;
-
-const [teachers, setTeachers] = useState([])
 
 export const getTeachers = () => {
 
     useEffect (() => {
         axios.get(`${URL}/teachers`)
         .then((res) => {
-            setTeachers(res.data) 
-            return teachers
+            
+            return res.data
         })
         .catch((error) => {
             console.log(error);
